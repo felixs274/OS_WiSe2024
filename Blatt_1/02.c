22acbfbb96912264dadcf1c8a3eccd193cc24dcf
felixs274 <felix.scholzen27@gmail.com>
2024-10-17 11:03:59 +0200

    Aufgabe 2 aus Vorlesung

--- Blatt_1/02.c
+++ Blatt_1/02.c
@@ -1,13 +1,46 @@
+// Blatt 1
+// Aufgabe 2
+// Threads
+
 #include <stdio.h>
-#include <pthread.h> // Thread Lib
+#include <stdlib.h>
+#include <pthread.h>
 
-#define NUM_THREADS 2
+int sum; // Data shared by the Thread(s)
 
-void *printHello(void *threadID){
-    printf("Thread ID: %ld\n", (long)threadID);
+void *runner(void *param){ // Threads call this function
+    int i, upper = atoi(param);
+    sum = 0;
 
-    if(){
-        
+    for(i = 1; i <= upper; i++){
+        sum += 1;
     }
+
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[]){
+
+    pthread_t tid; // Thread identifier
+    pthread_attr_t attr; // Set of Thread attributes
+
+    if(argc != 2){
+        fprintf(stderr, "usage: a.out <int>\n");
+    }
+
+    // atoi == ASCII to Int
+    if(atoi(argv[1]) < 0){
+        fprintf(stderr, "%d must be >= 0\n", atoi(argv[1]));
+        return -1;
+    }
+
+    // Get Default Attributes
+    pthread_attr_init(&attr);
+    // Create the Thread
+    pthread_create(&tid, &attr, runner, argv[1]);
+    // Wait fot Thread to exit
+    pthread_join(tid, NULL);
+
+    printf("sum = %d\n", sum);
 
 }
