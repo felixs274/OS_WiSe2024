3f540115e419673901e0d88f30e36230c8030830
felixs274 <felix.scholzen27@gmail.com>
2024-10-17 10:50:14 +0200

    Blatt 1 hinzugefügt

---
+++ Blatt_1/01.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int a = 5;
+
+int main(){
+    pid_t pid;
+
+    // Create Child Process
+    pid = fork();
+
+    if(pid < 0){ // Error
+        fprintf(stderr, "Ford Failed");
+        return 1;
+    } 
+    else if(pid == 0){ // Child Process
+        //execlp("/bin/ls", "ls", NULL);
+        printf("Child PID: %d\n", getpid());
+        printf("Var a: %d+1\n", a);
+        a += 1;
+    } 
+    else { // Parent Process
+        // Parent will wait for Child to complete
+        wait(NULL);
+        printf("\nChild complete\n\n");
+        printf("Parent PID: %d\n", getpid());
+        printf("Var a: %d\n", a);
+    }
+}
---
+++ Blatt_1/02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <pthread.h> // Thread Lib
+
+#define NUM_THREADS 2
+
+void *printHello(void *threadID){
+    printf("Thread ID: %ld\n", (long)threadID);
+
+    if(){
+        
+    }
+
+}
